2337f18543e8baa5c9404811f9fce60f02660db9
lyaakakaka <lyyyaaa04@gmail.com>
2026-04-19 22:00:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want to say goodluck on your exams.\n\nJangan tido lambat okay? Jangan main ml banyak sangat tauu. Try to focus on ur studyyy and try ur best to get straight A's !!.\n\nI really hope you succeed in life and get everything that you've ever wished for and maybe one day u get the person u've always dreamed of too? Hahah.\n\nAnyways, I hope you're doing okay, and I believe that u can do it!! Baca bismillah and don't forget to solat & doa.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
